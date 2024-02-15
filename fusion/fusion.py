a2b0239bd96027b0d40441dc1beb08b377332a97
Shannon Russell <uncoded0123@yahoo.com>
2024-02-14 19:57:06 -0600

    removed the unused commented out code

--- fusion/fusion.py
+++ fusion/fusion.py
@@ -1,42 +1,5 @@
 # # instructions for vosk: https://medium.com/analytics-vidhya/offline-speech-recognition-made-easy-with-vosk-c61f7b720215
-
-# from speech_to_text import ContinuousSpeechToText
-# import json
-
-# def main():
-#     cstt = ContinuousSpeechToText()
-
-#     def web(url):
-#         import webbrowser
-#         print(f'url {url}')
-#         webbrowser.open(f'https://www.{url}.com')
-
-#     try:
-#         while True:
-#             result_json = cstt.run()
-#             if result_json:
-#                 result = json.loads(result_json)
-#                 text = result.get('text', '').strip().lower()  # Use final result and normalize case
-#                 print(f'text {text}')
-#                 if ('fusion' in text) or ('asian' in text):
-#                     print('said fusion')
-#                     if 'go to' in text:
-#                         rm_fusion = text[7:]
-#                         rm_go_to = rm_fusion[6:]
-#                         rm_dot_com = rm_go_to[:-8]
-#                         web(url=rm_dot_com)
-
-#     except KeyboardInterrupt:
-#         print("Application stopped.")
-#     except Exception as e:
-#         print(f"Error occurred: {e}")
-#     finally:
-#         cstt.stop()
-
-# if __name__ == "__main__":
-#     main()
-
-
+# change device and sample rate in speech_to_text,py if not working
 
 from speech_to_text import ContinuousSpeechToText
 import json, webbrowser
