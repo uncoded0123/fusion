ca66cc1e22fa4d3ae86d28a77a523e229fb95e9d
Shannon Russell <uncoded0123@yahoo.com>
2024-02-18 22:45:46 -0600

    added text to voice, and gpt class

--- fusion/fusion.py
+++ fusion/fusion.py
@@ -5,26 +5,37 @@ from speech_to_text import ContinuousSpeechToText
 from dotenv import load_dotenv
 import json, webbrowser, openai
 import os
+import pyttsx3
 
 
 
-load_dotenv()  # Load .env file
-api_key = os.getenv('OPENAI_API')  # Access variable
+class GPT:
+    def __init__(self):
+        load_dotenv()  # Load .env file
+        self.api_key = os.getenv('OPENAI_API')  # Access variable
+        self.client = openai.OpenAI(api_key=self.api_key)
 
-def gpt(prompt):
-    client = openai.OpenAI(api_key=api_key)
-    stream = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": f"clear and concise as possible, in less than 20 words, {prompt}"}],
-        stream=True,
-        max_tokens=50)
-    answer_lst = []
-    for chunk in stream:
-        if chunk.choices[0].delta.content is not None:
-            answer = chunk.choices[0].delta.content
-            answer_lst.append(answer)
-            # print(chunk.choices[0].delta.content, end="")
-    return ''.join(answer_lst)
+    def text_generator(self, prompt):
+        stream = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": f"clear and concise as possible, in less than 20 words, {prompt}"}],
+            stream=True,
+            max_tokens=50)
+        answer_lst = []
+        for chunk in stream:
+            if chunk.choices[0].delta.content is not None:
+                answer = chunk.choices[0].delta.content
+                answer_lst.append(answer)
+                # print(chunk.choices[0].delta.content, end="")
+        return ''.join(answer_lst)
+    
+
+def text_to_speech(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 100)
+    # engine.setProperty('voice', engine.getProperty('voices')[17].id)
+    engine.say(f"-h {text}")
+    engine.runAndWait()
 
 
 def main():
@@ -35,15 +46,18 @@ def main():
             if result:
                 text = json.loads(result).get('text', '').lower()
                 print(f'text {text}')
-                if ('fusion' in text) or ('huge' in text):
+                if ('fusion' in text) or ('eugune' in text):
                     if 'go to' in text:
                         domain_name = text[13:][:-8].replace(' ','')
                         webbrowser.open(f"https://www.{domain_name}.com")
                     if ('what' in text) or ('how' in text):
-                        print(gpt(f'{text[7:]}'))
+                        txt = gpt1.text_generator(f'{text[7:]}')
+                        text_to_speech(txt)
+
 
     except KeyboardInterrupt: print("Application stopped.")
     except Exception as e: print(f"Error occurred: {e}")
 
 if __name__ == "__main__":
+    gpt1 = GPT()
     main()
