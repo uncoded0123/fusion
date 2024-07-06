12dadce6c1293eac0af0c7b184e3d9edd95eede8
Shannon Russell <uncoded0123@yahoo.com>
2024-07-06 00:07:42 -0500

    combined 2 files, added classes

--- fusion/fusion.py
+++ fusion/fusion.py
@@ -1,19 +1,54 @@
 # # instructions for vosk: https://medium.com/analytics-vidhya/offline-speech-recognition-made-easy-with-vosk-c61f7b720215
 # change device and sample rate in speech_to_text,py if not working
 
-from speech_to_text import ContinuousSpeechToText
 from dotenv import load_dotenv
 import json, webbrowser, openai
 import os
 import pyttsx3
 import serial
 import requests
-
-
+import queue
+import sys
+import sounddevice as sd
+from vosk import Model, KaldiRecognizer
 import serial.tools.list_ports
-ports = serial.tools.list_ports.comports()
-for port, desc, hwid in sorted(ports):
-        print(f"{port}: {desc} [{hwid}]")
+
+
+
+class ContinuousSpeechToText:
+    def __init__(self, lang="en-us", samplerate=48000, device_index=4):  # laptop webcam audio samplerate=48000 device_index=4, desktop=?
+        self.q = queue.Queue()
+        self.model = Model(lang=lang)
+        self.samplerate = samplerate
+        self.recognizer = KaldiRecognizer(self.model, self.samplerate)
+        self.stream = sd.RawInputStream(samplerate=self.samplerate, blocksize=8000, device=device_index, dtype="int16", channels=1, callback=self.audio_callback)  # Use device_index here
+        self.stream.start()
+
+    def audio_callback(self, indata, frames, time, status):
+        if status:
+            print(status, file=sys.stderr)
+        self.q.put(bytes(indata))
+
+    def run(self):
+        if not self.q.empty():
+            data = self.q.get()
+            if self.recognizer.AcceptWaveform(data):
+                return self.recognizer.Result()
+            else:
+                return self.recognizer.PartialResult()
+
+    def stop(self):
+        self.stream.stop()
+        self.stream.close()
+
+
+class CheckPorts:
+    def __init__(self):
+        ports = serial.tools.list_ports.comports()
+
+    def check(self):
+        for port, desc, hwid in sorted(ports):
+            print(f"{port}: {desc} [{hwid}]")
 
 
 class Serial1:
@@ -24,9 +59,6 @@ class Serial1:
         pass
     # def power(self, x='off'):
     #     self.ser1.write(str(x).encode())
-
-
-# Serial1().power('off')
 
 
 class GPT:
@@ -52,50 +84,60 @@ class GPT:
         return ''.join(answer_lst)
     
 
-def text_to_speech(text):
-    engine = pyttsx3.init("espeak")
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice',voices[11].id) #English
-    engine.setProperty('rate', 110)
-    engine.say(f"-h {text}")
-    engine.runAndWait()
+class TextToSpeech:
+    def __init__(self):
+        pass
+
+    def text_to_speech(self, text):
+        engine = pyttsx3.init("espeak")
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice',voices[11].id) #English
+        engine.setProperty('rate', 110)
+        engine.say(f"-h {text}")
+        engine.runAndWait()
 
 
-def main():
-    cstt = ContinuousSpeechToText()
-    try:
-        while True:
-            result = cstt.run()
-            if result:
-                text = json.loads(result).get('text', '').lower()
-                print(f'text {text}')
-                if ('hey fusion' in text) or ('a fusion' in text):
+class Main:
+    def __init__(self):
+        pass
 
-                    if 'go to' in text:
-                        domain_name = text[17:][:-8].replace(' ','')
-                        webbrowser.open(f"https://www.{domain_name}.com")
+    def main(self):
+        cstt = ContinuousSpeechToText()
+        tts = TextToSpeech()
+        try:
+            while True:
+                result = cstt.run()
+                if result:
+                    text = json.loads(result).get('text', '').lower()
+                    print(f'text {text}')
+                    if ('hey fusion' in text) or ('a fusion' in text):
 
-                    elif ('punk' in text) or ('park' in text):
-                        text_to_speech("hey, don't call me punk")
+                        if 'go to' in text:
+                            domain_name = text[17:][:-8].replace(' ','')
+                            webbrowser.open(f"https://www.{domain_name}.com")
 
-                    elif (('what' in text) or ('how' in text) or ('is' in text)
-                           or ('tell' in text) or ('you' in text)):
-                        txt = gpt_obj.text_generator(f'{text[11:]}')
-                        text_to_speech(txt)
-                    
-                    elif 'power on' in text:
-                        requests.get(f"{os.getenv('URL')}/data_test1/on")
-                        # ser_obj.power('on' if ' on' in text else 'off')
-                    
-                    elif "power off" in text:
-                        requests.get(f"{os.getenv('URL')}/data_test1/off")
+                        elif ('punk' in text) or ('park' in text):
+                            tts.text_to_speech("hey, don't call me punk")
+
+                        elif (('what' in text) or ('how' in text) or ('is' in text)
+                            or ('tell' in text) or ('you' in text)):
+                            txt = gpt_obj.text_generator(f'{text[11:]}')
+                            tts.text_to_speech(txt)
+                        
+                        elif 'power on' in text:
+                            requests.get(f"{os.getenv('URL')}/data_test1/on")
+                            # ser_obj.power('on' if ' on' in text else 'off')
+                        
+                        elif "power off" in text:
+                            requests.get(f"{os.getenv('URL')}/data_test1/off")
+
+        except KeyboardInterrupt: print("Application stopped.")
+        except Exception as e: print(f"Error occurred: {e}")
 
 
-
-    except KeyboardInterrupt: print("Application stopped.")
-    except Exception as e: print(f"Error occurred: {e}")
 
 if __name__ == "__main__":
     gpt_obj = GPT()
     ser_obj = Serial1()
-    main()
+    m = Main()
+    m.main()
