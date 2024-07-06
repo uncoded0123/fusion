fef0c92ab5733d169cc9e11f8bcfc633aec5b750
Shannon Russell <uncoded0123@yahoo.com>
2024-07-06 00:14:39 -0500

    lots of changes

--- esp32/in_from_internet_board/connect.py
+++
@@ -1,7 +0,0 @@
-import time, network, ssid_password
-station = network.WLAN(network.STA_IF)
-station.active(True)
-while station.isconnected() == False:
-    station.connect(ssid_password.SSID, ssid_password.PASSWORD)
-    time.sleep(1)
-print("Connected!")
--- esp32/in_from_internet_board/main.py
+++
@@ -1,7 +0,0 @@
-import time, connect, send_recieve, servo
-#test 2.1
-while True:
-    if send_recieve.recieve() == 'detected':
-        servo.s_on('on')
-        send_recieve.send('off')
-    time.sleep(0.2)
